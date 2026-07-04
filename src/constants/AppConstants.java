16e794801e412a65c2d0ffb3e6e2999e2e12a0d9
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-07-04 23:19:46 +0530

    appconstant file was modified

--- src/constants/AppConstants.java
+++ src/constants/AppConstants.java
@@ -23,9 +23,9 @@ public final class AppConstants {
     public static final String DB_URL =
             "jdbc:mysql://localhost:3306/college_erp";
 
-    public static final String DB_USERNAME = "root";
+    public static final String DB_USERNAME = "your_username";
 
-    public static final String DB_PASSWORD = "jaideep@2006";
+    public static final String DB_PASSWORD = "your_password";
 
     // ==========================
     // MENU
